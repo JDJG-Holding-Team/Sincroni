ba7787667614be9ee1466fbf13e33203fea36d42
Soheab_ <33902984+Soheab@users.noreply.github.com>
2023-02-24 17:55:28 +0100

    run black -l120 .

--- cogs/jsk.py
+++ cogs/jsk.py
@@ -9,9 +9,10 @@ from jishaku.repl import AsyncCodeExecutor, get_var_dict_from_ctx
 # look into making more jishaku commands: https://jishaku.readthedocs.io/en/latest/cog.html
 
 
+# fmt: off
 class Jishaku(*OPTIONAL_FEATURES, *STANDARD_FEATURES):
     @Feature.Command(parent="jsk", name="py", aliases=["python"])
-    async def jsk_python(self, ctx: commands.Context, *, argument: codeblock_converter):
+    async def jsk_python(self, ctx: commands.Context, *, argument: codeblock_converter):  # type: ignore
 
         arg_dict = get_var_dict_from_ctx(ctx, "")
         arg_dict.update(get_var_dict_from_ctx(ctx, "_"))
@@ -23,7 +24,7 @@ class Jishaku(*OPTIONAL_FEATURES, *STANDARD_FEATURES):
             async with ReplResponseReactor(ctx.message):
                 with self.submit(ctx):
                     executor = AsyncCodeExecutor(argument.content, scope, arg_dict=arg_dict)
-                    async for send, result in AsyncSender(executor):
+                    async for send, result in AsyncSender(executor):  # type: ignore
                         if result is None:
                             continue
 
@@ -33,6 +34,8 @@ class Jishaku(*OPTIONAL_FEATURES, *STANDARD_FEATURES):
 
         finally:
             scope.clear_intersection(arg_dict)
+
+# fmt: on
 
 
 async def setup(bot: commands.Bot):
