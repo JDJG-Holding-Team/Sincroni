88f3e2b3029535b62772c56c4b4510a169ff229e
JDJG <ghostzap123@gmail.com>
2023-11-16 02:41:54 -0500

    Quick fix

--- utils/database/models.py
+++ utils/database/models.py
@@ -127,7 +127,7 @@ class Blacklist:
 
 
     @property
-    def _global (self) -> bool:
+    def _global(self) -> bool:
         """Whether or not the blacklist is enforced at a server or blacklist level."""
         return bool(self.server_id)
 
