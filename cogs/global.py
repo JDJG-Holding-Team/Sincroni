48f59c16bea68eb54aa4a06588859ce683d3ac13
JDJG <ghostzap123@gmail.com>
2024-02-18 00:49:05 -0500

    Try this?

--- cogs/global.py
+++ cogs/global.py
@@ -217,7 +217,7 @@ class Global(commands.Cog):
         self,
         ctx: commands.Context,
         user: Optional[discord.User],
-        guild: Optional[int],
+        guild: Optional[str],
         reason: Optional[str],
         public: bool = True,
         developer: bool = True,
@@ -232,7 +232,10 @@ class Global(commands.Cog):
         if not check_owner:
             return await ctx.send("Sorry you must be owner to run this command for the time being.")
 
-        valid_guild = bot.get_guild(guild)
+        if not guild.isint():
+            return await ctx.send("That's not a valid guild, please try again.")
+
+        valid_guild = bot.get_guild(int(guild))
 
         if guild and not user and not valid_guild:
             return await ctx.send("That guild does not exist sadly.")
@@ -256,7 +259,7 @@ class Global(commands.Cog):
 
         records = [record for record in self.bot.db.global_chats if record.guild and record.server_id != interaction.guild_id]
 
-        guilds = [app_commands.Choice(name=f"{record.guild}", value=record.server_id) for record in records]
+        guilds = [app_commands.Choice(name=f"{record.guild}", value=str(record.server_id)) for record in records]
         startswith: list[Choice] = [choices for choices in guilds if guilds.name.startswith(current)]
         if not (current and startswith):
             return guilds[0:25]
